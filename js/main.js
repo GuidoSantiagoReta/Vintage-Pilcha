69570c322613f05b306989f5658c59595a48dfdf
GuidoSantiagoReta <guidoreta@hotmail.com>
2022-09-08 20:11:15 -0300

    compression

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
 
 const  navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    
-    burger.addEventListener('click',()=>{
-        //toggle Nav
-         nav.classList.toggle('nav-active');
-    
-        //Animate links
-        navLinks.forEach((link, index) => {
-          if(link.style.animation){
-            link.style.animation = '';
-          } else{
-            link.style.animation = `navLinkFade 0.2s ease forwards ${index / 7 + 0}s`;
-        }
-      });
-      //Burger Animation
-      burger.classList.toggle('toggle');
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+  
+  burger.addEventListener('click',()=>{
+      //toggle Nav
+       nav.classList.toggle('nav-active');
+  
+      //Animate links
+      navLinks.forEach((link, index) => {
+        if(link.style.animation){
+          link.style.animation = '';
+        } else{
+          link.style.animation = `navLinkFade 0.2s ease forwards ${index / 7 + 0}s`;
+      }
     });
-   
- }
- navSlide();
+    //Burger Animation
+    burger.classList.toggle('toggle');
+  });
+ 
+}
+navSlide();
 
- /*BUTTON*/
+/*BUTTON*/
 
- const btn = document.querySelector(".btn");
+const btn = document.querySelector(".btn");
 
- function changeColor (color){
-  btn.style.color = color;
- }
+function changeColor (color){
+btn.style.color = color;
+}
 
  
